6bdcca60f40baa6758d323179d09919653b8611d
Prio <s333027@studenti.polito.it>
2024-05-27 18:34:35 +0200

    added custom transform functions for images and labels

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -2,12 +2,26 @@ import os
 import torch
 import numpy as np
 
+from numba import njit
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor, Resize, Compose, Normalize
 from PIL import Image
 
+@njit
+def convert_label(label):
+    # Iterate over each pixel value and assign 255 if it is not in the labels
+    for i in range(label.shape[0]):
+        for j in range(label.shape[1]):
+            if label[i][j] > 18:
+                label[i][j] = 255
+
+    # Return the labels
+    return label
+
 class CityScapes(Dataset):
-    def __init__(self, root_dir, split='train', mode='multiple', raw_label=False):
+    def __init__(self, root_dir, 
+                 split='train', mode='multiple', raw_label=False,
+                 custom_transform_image=None, custom_transform_label=None):
         super(CityScapes, self).__init__()
         # Save the root directory of the dataset
         self.root_dir = root_dir
@@ -15,6 +29,9 @@ class CityScapes(Dataset):
         self.mode = mode
         # Save the label raw mode
         self.raw_label = raw_label
+        # Save the custom transformations
+        self.custom_transform_image = custom_transform_image
+        self.custom_transform_label = custom_transform_label
         # Define the transformations for the label
         self.transform_label = Compose([
             Resize((512, 1024)),
@@ -35,41 +52,6 @@ class CityScapes(Dataset):
         # Get the list of cities
         self.cities = sorted(os.listdir(self.image_dir))
 
-    def convert_from_image_to_label(self, image):
-        # Convert the image to a numpy array
-        image = np.array(image)
-
-        # Create a new image where each pixel is labeled
-        labels = {
-            0: 0,   # road
-            1: 1,   # sidewalk
-            2: 2,   # building
-            3: 3,   # wall
-            4: 4,   # fence
-            5: 5,   # pole
-            6: 6,   # traffic light
-            7: 7,   # traffic sign
-            8: 8,   # vegetation
-            9: 9,   # terrain
-            10: 10, # sky
-            11: 11, # person
-            12: 12, # rider
-            13: 13, # car
-            14: 14, # truck
-            15: 15, # bus
-            16: 16, # train
-            17: 17, # motorcycle
-            18: 18, # bicycle
-        }
-        # Iterate over each pixel value and assign 255 if it is not in the labels
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                if image[i][j] > 18:
-                    image[i][j] = 255
-
-        # Convert the image to a tensor
-        return torch.tensor(image, dtype=torch.long)
-
     def __getitem__(self, idx):
         # Find the city and image index
         for city in self.cities:
@@ -84,7 +66,13 @@ class CityScapes(Dataset):
             idx -= len(images)
 
         # Load the image and label
-        image = self.transform_image(Image.open(os.path.join(self.image_dir, city, images[idx])))
+        # Check if there are custom transformations
+        if self.custom_transform_image != None:
+            # Load the image with the custom transformation
+            image = self.custom_transform_image(Image.open(os.path.join(self.image_dir, city, images[idx])))
+        else:
+            # Load the image with the default transformation
+            image = self.transform_image(Image.open(os.path.join(self.image_dir, city, images[idx])))
 
         # Load the label
         if self.mode == 'multiple':
@@ -95,7 +83,9 @@ class CityScapes(Dataset):
             label = self.transform_label(Image.open(os.path.join(self.label_dir, city, images[idx].replace('leftImg8bit', 'gtFine_labelTrainIds'))))
             if self.raw_label == False:
                 # Convert the label to the correct format
-                label = self.convert_from_image_to_label(label)
+                label = convert_label(np.array(label))
+                # Transform the label to a tensor
+                label = torch.tensor(label).long()
 
         # Return the image and label
         return image, label
